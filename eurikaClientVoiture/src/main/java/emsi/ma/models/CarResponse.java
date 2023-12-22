90186e4f640532ab72bf78df8cb1f15fa46fd895
Samiha ELHAJJAM <samihaelhajjam2001@gmail.com>
2023-12-22 22:38:04 +0100

    first commit

---
+++ eurikaClientVoiture/src/main/java/emsi/ma/models/CarResponse.java
@@ -0,0 +1,19 @@
+package emsi.ma.models;
+
+import emsi.ma.entities.Client;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CarResponse {
+    private Long id;
+    private String brand;
+    private String model;
+    private String matricule;
+    private Client client;
+}
---
+++ eurikaClientVoiture/src/test/java/emsi/ma/cars/CarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package emsi.ma.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
